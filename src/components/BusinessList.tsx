b83a4661ca78ab98873f1cf435c372451ab2c668
David Zhang <davidzhang2023@u.northwestern.edu>
2022-04-30 18:33:01 -0500

    tested with fake data, started businesslist

---
+++ src/components/BusinessList.tsx
@@ -0,0 +1,20 @@
+import { Business } from "./Business";
+import { Stack } from "react-bootstrap";
+
+import { BusinessEntry } from "../types/BusinessTypes";
+
+type BusinessListProps = {
+    businessList: BusinessEntry[]
+}
+
+const filteredBusinesses = () => {
+    
+}
+
+export const BusinessList = (props: BusinessListProps ) => {
+    return (
+        <Stack direction="vertical">
+            
+        </Stack>
+    );
+}
