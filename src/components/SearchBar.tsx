1e5e8deda43abefa2bbbe233241bdfc6dac253a2
yfarge <83893088+yfarge@users.noreply.github.com>
2022-05-07 18:21:31 -0500

    search now works for substrings found in business tags, titles, and descriptions

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,5 @@
-import { Stack, Button, Form } from "react-bootstrap";
-import { useState } from 'react';
+import { Button, Form, Stack } from "react-bootstrap";
 import { BusinessEntry } from '../types/BusinessTypes';
-import { BusinessList } from '../components/BusinessList';
 
 type SearchBarProps = {
     searchText: string,
@@ -12,39 +10,18 @@ type SearchBarProps = {
 export const SearchBar = ({ searchText, setSearchText, businessList }: SearchBarProps) => {
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        const filteredText = searchText.split(' ');
-        // const filteredBusinesses = Object.values(businessList).filter(business =>
-        //     business["Search Tags"].map(tag =>
-        //         tag.toLowerCase()).includes(searchText.toLowerCase()));
-        /*
-            "Ruby Nails".split(' ').map(text => obj["Search Tags"].includes(text.toLowerCase())).includes(true)
-        */
-        // const filteredBusinesses = [...searchText.split(' '), searchText].map(text => 
-        //     Object.values(businessList).filter(business => 
-        //         business["Search Tags"].includes(text.toLowerCase())));
-
-        // [...searchText.split(' '), searchText] // [Ruby, Nails, Ruby Nails]
-
-        // const isTag = () => { }
 
         const intersect = (keywords: Array<string>, tags: Array<string>) => (
             keywords.filter(keyword => tags.some(tag => tag.includes(keyword)))
         );
-        const arr1 = ["Hello World!", "Javascript", "TypeScript", "ode"];
-        const arr2 = ["Hello World!", "Python", "Business"];
-        const arr3 = ["ello", "Python", "Business"];
-        const arr4 = ["Python", "Business", "Code"];
-        console.log(intersect(arr1, arr2));
-        console.log(intersect(arr1, arr3));
-        console.log(intersect(arr1, arr4));
-        console.log(intersect(arr3, arr1));
+
+        const filteredText = searchText.split(' ');
 
         const filteredBusinesses = Object.values(businessList).filter(business =>
-            (business["Search Tags"].concat([business.Title, business.Description])).map(tag =>
-                tag.toLowerCase().includes(searchText.toLowerCase())).includes(true));
+            intersect(filteredText.map(text =>
+                text.toLowerCase()), business["Search Tags"].concat([business.Title, business.Description]).map(text =>
+                    text.toLowerCase())).length > 0);
         console.log(filteredBusinesses);
-
-
     };
 
     return (
--- src/components/SearchPage.tsx
+++ src/components/SearchPage.tsx
@@ -1,24 +1,24 @@
-import { Form } from 'react-bootstrap';
 import { BasicFilter } from './BasicFilter';
 import { AdvancedSearch } from './AdvancedSearch';
 import { SearchBar } from './SearchBar';
 import { useState } from 'react';
+import { BusinessEntry } from '../types/BusinessTypes';
 
 type SearchPageProps = {
-    businessList: any;
+    businessList: BusinessEntry[];
 };
-        
+
 export const SearchPage = (props: SearchPageProps) => {
     const [searchText, setSearchText] = useState('');
     const [filterValues, setFilterValues] = useState<string[]>([] as string[]);
 
-    return(
+    return (
         <>
-            <p style={{textAlign: "center"}}>Find small businesses near you that support your values</p>
+            <p style={{ textAlign: "center" }}>Find small businesses near you that support your values</p>
             <SearchBar searchText={searchText} setSearchText={setSearchText} businessList={props.businessList} />
             <br></br>
             {/* <BasicFilter filterValues={filterValues} setFilterValues={setFilterValues}/> */}
-            <AdvancedSearch filterValues={filterValues}/>
+            <AdvancedSearch filterValues={filterValues} />
         </>
     );
 };
