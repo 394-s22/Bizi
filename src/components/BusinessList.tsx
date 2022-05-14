f0f665476bc24ed13d309fac3de3a9662bbee218
jamie7533 <lee.jamie.july@gmail.com>
2022-05-14 18:11:54 -0500

    added getBasicFilters and sorted business results

--- src/components/BusinessList.tsx
+++ src/components/BusinessList.tsx
@@ -2,6 +2,8 @@ import type { ReactNode } from "react";
 import { Stack } from "react-bootstrap";
 import { BusinessEntry } from "../types/BusinessTypes";
 import { Business } from "./Business";
+import { getBasicFilters } from "../utilities/filtering"
+
 
 type BusinessListProps = {
   businessList: BusinessEntry[];
@@ -14,6 +16,21 @@ export const BusinessList: React.FC<BusinessListProps> = ({ businessList }) => {
   // need to pass in the list of basic/advanced filter values that were selected 
   // prioritize businesses that fulfill the most BASIC filters 
   // then sort by the number of advanced filters satisfied (regardless of the category)
+  // get # basic filters satisfied 
+  // get # advanced filter satisfied 
+  // (x, y)
+  filteredBusinesses.sort((business1: BusinessEntry, business2 : BusinessEntry) => { 
+    const basicFilters1 = getBasicFilters(business1).length;
+    const basicFilters2 = getBasicFilters(business2).length;
+
+    if (basicFilters1 < basicFilters2) return 1;
+    if (basicFilters2 < basicFilters1) return -1;
+    
+    if (business1.Initiatives?.length < business2.Initiatives?.length) return 1;
+    if (business2.Initiatives?.length < business1.Initiatives?.length) return -1;
+    else return 1; 
+  })
+  
   return (
     <Stack direction="vertical">
       {Object.values(filteredBusinesses).map<ReactNode>(
