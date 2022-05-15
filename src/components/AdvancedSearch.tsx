c9aeeb299d9cd6e341f3f06f9035ac6cb134b312
jamie7533 <lee.jamie.july@gmail.com>
2022-05-14 19:03:18 -0500

    formatting

--- src/components/AdvancedSearch.tsx
+++ src/components/AdvancedSearch.tsx
@@ -1,10 +1,10 @@
-import { useEffect } from 'react';
-import { Button, ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
-import { FaAngleLeft } from 'react-icons/fa';
-import valueData from '../data/values.json';
+import { useEffect } from "react";
+import { Button, ToggleButton, ToggleButtonGroup } from "react-bootstrap";
+import { FaAngleLeft } from "react-icons/fa";
+import valueData from "../data/values.json";
 
 type AdvancedSearchProps = {
-  filterValues: string[],
+  filterValues: string[];
   setFilterValues: React.Dispatch<React.SetStateAction<string[]>>;
   searchComponent: string;
   setSearchComponent: React.Dispatch<React.SetStateAction<string>>;
@@ -16,13 +16,13 @@ const valueDictionary: { [key: string]: string[] } = valueData;
 
 export const AdvancedSearch = (props: AdvancedSearchProps) => {
   const buttonStyle: React.CSSProperties = {
-    backgroundColor: '#ddd',
-    border: 'none',
-    color: 'black',
-    padding: '5px 10px',
-    textAlign: 'center',
-    margin: '2px 1px',
-    borderRadius: '16px',
+    backgroundColor: "#ddd",
+    border: "none",
+    color: "black",
+    padding: "5px 10px",
+    textAlign: "center",
+    margin: "2px 1px",
+    borderRadius: "16px",
   };
 
   useEffect(() => {
@@ -35,8 +35,8 @@ export const AdvancedSearch = (props: AdvancedSearchProps) => {
   }, [props.advancedFilterValues]);
 
   const changeBasicFilter = (group_elements: string[], basic: string) => {
-    return group_elements.every(e => props.advancedFilterValues.includes(e));
-  }
+    return group_elements.every((e) => props.advancedFilterValues.includes(e));
+  };
 
   const toggleButton = (val: string) => {
     const state = props.advancedFilterValues.includes(val);
@@ -49,10 +49,10 @@ export const AdvancedSearch = (props: AdvancedSearchProps) => {
 
   return (
     <>
-      <div className='mx-2'>
+      <div className="mx-2">
         <Button
-          variant='light'
-          onClick={() => props.setSearchComponent('basic')}
+          variant="light"
+          onClick={() => props.setSearchComponent("basic")}
         >
           <FaAngleLeft />
           Back to Quick Filter
@@ -60,22 +60,19 @@ export const AdvancedSearch = (props: AdvancedSearchProps) => {
       </div>
       {Object.entries(valueDictionary).map((entry, catID) => {
         return (
-          <div key={catID} className='mx-4 mb-2'>
+          <div key={catID} className="mx-4 mb-2">
             <h1>{entry[0]}</h1>
-            <ToggleButtonGroup 
-              type='checkbox'
-              style={{ flexWrap: 'wrap' }}
-            >
+            <ToggleButtonGroup type="checkbox" style={{ flexWrap: "wrap" }}>
               {entry[1].map((value, valID) => {
                 const state = props.advancedFilterValues.includes(value);
                 return (
                   <ToggleButton
                     key={valID}
-                    className='mx-1'
+                    className="mx-1"
                     style={{
                       ...buttonStyle,
-                      backgroundColor: state ? 'black' : '#ddd',
-                      color: state ? 'white' : 'black',
+                      backgroundColor: state ? "black" : "#ddd",
+                      color: state ? "white" : "black",
                     }}
                     value={value}
                     onClick={() => {
