3c75799d25eddb0db1daa8f3338d207b12c3340f
jamie7533 <lee.jamie.july@gmail.com>
2022-06-04 18:52:01 -0500

    deployed scrolling fix, removed firebase config afterwards

--- src/utilities/firebase.ts
+++ src/utilities/firebase.ts
@@ -4,14 +4,7 @@ import { useEffect, useState } from "react";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB5MB0_czvXQceLJXSprAbrF6z_Wv6SIKY",
-  authDomain: "bizi-aabc7.firebaseapp.com",
-  databaseURL: "https://bizi-aabc7-default-rtdb.firebaseio.com",
-  projectId: "bizi-aabc7",
-  storageBucket: "bizi-aabc7.appspot.com",
-  messagingSenderId: "791491230288",
-  appId: "1:791491230288:web:91c69688744f2b59602f55",
-  measurementId: "G-3DLW360CDE",
+// insert firebaseConfig from Project Settings here
 };
 
 // Initialize Firebase
